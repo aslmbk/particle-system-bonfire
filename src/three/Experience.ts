49f19793bb3add1c9c3df4815b9986f8793b4c07
aslmbk <assayf95@gmail.com>
2025-05-29 23:20:33 +0300

    init project

--- src/three/Experience.ts
+++ src/three/Experience.ts
@@ -1,58 +1,33 @@
 import { Engine } from "./engine";
 import { DebugController } from "./DebugController";
 import { Config } from "./Config";
-import * as THREE from "three";
-import { SelectiveBloom } from "./SelectiveBloom";
+import { ParticlesManager } from "./ParticlesManager";
 
 export class Experience extends Engine {
-  public readonly config: Config;
-  public readonly debugController: DebugController;
+  private static instance: Experience | null = null;
 
-  private selectiveBloom: SelectiveBloom;
+  public readonly config!: Config;
+  public readonly debugController!: DebugController;
 
   constructor(domElement: HTMLElement) {
-    super({ domElement, autoRender: false });
+    if (Experience.instance) return Experience.instance;
+    super({ domElement });
+    Experience.instance = this;
+
     this.config = new Config();
-    this.debugController = new DebugController(this);
+    this.debugController = new DebugController();
 
-    this.renderer.setClearColor(this.config.clearColor);
+    this.stats.activate();
+    this.camera.position.set(0, 0, 20);
 
-    this.selectiveBloom = new SelectiveBloom(
-      this.renderer,
-      this.scene,
-      this.camera
-    );
+    const particlesManager = new ParticlesManager(this.loader, this.viewport);
+    this.scene.add(particlesManager.scene);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube1 = new THREE.Mesh(geometry, material);
-    cube1.position.x = -2;
-    this.scene.add(cube1);
-
-    const cube2 = new THREE.Mesh(geometry, material);
-    cube2.position.x = 2;
-    this.selectiveBloom.toggleBloom(cube2);
-    this.scene.add(cube2);
-
-    this.time.events.on(
-      "tick",
-      () => {
-        this.selectiveBloom.render();
-      },
-      5
-    );
-    this.viewport.events.on("change", ({ width, height }) => {
-      this.selectiveBloom.resize(width, height);
+    this.time.events.on("tick", (time) => {
+      particlesManager.update(time);
     });
-  }
-
-  public dispose() {
-    this.debug.children.forEach((child) => child.dispose());
-    this.scene.traverse((child) => {
-      if (child instanceof THREE.Mesh) {
-        child.geometry.dispose();
-        child.material.dispose();
-      }
+    this.viewport.events.on("change", (viewport) => {
+      particlesManager.resize(viewport);
     });
   }
 }
